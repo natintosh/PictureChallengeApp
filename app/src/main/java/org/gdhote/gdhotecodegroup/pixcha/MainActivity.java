9101b5a2a1b55afdb891e7b9404e312a45e3ea83
Oluwatobiloba Ogunye <ogunye4nao@gmail.com>
2018-10-05 19:49:56 +0100

    Second commit: Making App interactive with user defined events

--- app/src/main/java/org/gdhote/gdhotecodegroup/pixcha/MainActivity.java
+++ app/src/main/java/org/gdhote/gdhotecodegroup/pixcha/MainActivity.java
@@ -1,23 +1,71 @@
 package org.gdhote.gdhotecodegroup.pixcha;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager mFragmentManager;
+    FeedsFragment mFeedsFragment;
+    ProfileFragment mProfileFragment;
+    private BottomNavigationView.OnNavigationItemSelectedListener mNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    int itemId = menuItem.getItemId();
+
+                    switch (itemId) {
+                        case R.id.nav_action_feeds:
+                            Toast.makeText(MainActivity.this, "Feeds", Toast.LENGTH_SHORT).show();
+                            mFeedsFragment = new FeedsFragment();
+                            loadFragment(mFeedsFragment);
+                            return true;
+                        case R.id.nav_action_camera:
+                            Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
+                            return true;
+                        case R.id.nav_action_profile:
+                            Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                            mProfileFragment = new ProfileFragment();
+                            loadFragment(mProfileFragment);
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FeedsFragment feedsFragment = new FeedsFragment();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mNavigationItemSelectedListener);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        mProfileFragment = new ProfileFragment();
+        mFeedsFragment = new FeedsFragment();
 
-        fragmentManager.beginTransaction()
-                .add(R.id.view_frame_layout, feedsFragment)
-                .commit();
+        loadFragment(mFeedsFragment);
+
+
     }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.view_frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
 }
---
+++ app/src/main/java/org/gdhote/gdhotecodegroup/pixcha/ProfileFragment.java
@@ -0,0 +1,47 @@
+package org.gdhote.gdhotecodegroup.pixcha;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
